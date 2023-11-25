139e106ae31dd90a3a8f2a31f3481d2b78e4e4c0
Andre Fragata <andrefragata14@gmail.com>
2023-11-25 12:31:35 -0300

    Linear Gradient funcionando

--- Simulacar/src/screens/Login/index.tsx
+++ Simulacar/src/screens/Login/index.tsx
@@ -1,6 +1,6 @@
 import { Text, TextInput, View } from "react-native";
 import { styles } from "./style";
-import LinearGradient from "react-native-linear-gradient";
+import {LinearGradient} from 'expo-linear-gradient';
 
 function Login(){
     return (
